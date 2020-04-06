b8c2bf35b23b10f109cb20c3752b97a81fb1e4a9
arhaiyun <vrhaiyun@gmail.com>
2020-04-06 23:05:34 +0800

    Spring AOP

--- src/main/java/com/exodus/config/MainConfigOfAOP.java
+++ src/main/java/com/exodus/config/MainConfigOfAOP.java
@@ -167,16 +167,16 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  *
  *
  *
- * 总结
+ * AOP总结
  *      1.通过@EnableAspectJAutoProxy开启AOP功能
  *      2.@EnableAspectJAutoProxy注解会给容器中注册一个AnnotationAwareAspectJAutoProxyCreator组件
  *      3.AnnotationAwareAspectJAutoProxyCreator 是一个后置处理器
  *      4.利用容器的创建流程
  *          a.registerBeanPostProcessors()注册后置处理器：创建AnnotationAwareAspectJAutoProxyCreator对象
  *          b.finishBeanFactoryInitialization() 初始化剩下的单实例bean
- *              1.创建业务逻辑组件和切面组件
- *              2.AnnotationAwareAspectJAutoProxyCreator拦截组件创建过程
- *              3.组件创建完成之后，判断组件是否需要增强 wrapIfNecessary
+ *              1).创建业务逻辑组件和切面组件
+ *              2).AnnotationAwareAspectJAutoProxyCreator拦截组件创建过程
+ *              3).组件创建完成之后，判断组件是否需要增强 wrapIfNecessary
  *                  需要增强：将切面的通知方法，包装成增强器advisor 给业务逻辑组件创建一个代理对象（cglib/JDK dynamic proxy）
  *      5.执行目标方法
  *          a.代理对象执行目标方法
@@ -184,7 +184,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  *              1).得到目标方法的拦截器链（advisors-->methodInterceptor）
  *              2).利用拦截器的链式机制，依次进入每一个拦截器进行执行
  *              3).效果：前置通知 => 目标方法 => 后置方法 => 返回通知（正常执行）/异常通知（执行产生异常）
- *
  *
  *
  */
