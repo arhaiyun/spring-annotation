b7ad991c897b0b88631fa33eccbfa9972b5a3bd6
arhaiyun <vrhaiyun@gmail.com>
2020-04-07 11:59:20 +0800

    Spring Transactions

--- src/main/java/com/exodus/tx/TxConfig.java
+++ src/main/java/com/exodus/tx/TxConfig.java
@@ -7,9 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionException;
-import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -25,6 +22,30 @@ import java.beans.PropertyVetoException;
  *      3.给方法上标注 @Transactional表示当前方法为一个事务方法 （UserService.java）
  *      4.配置类添加 @EnableTransactionManagement 开启基于注释的事务管理功能
  *      5.配置事务管理器来管理事务
+ *
+ *  Spring Transaction 原理:
+ *      1.利用@EnableTransactionManagement给容器中注册组件TransactionManagementConfigurationSelector
+ *          导入两个组件
+ *          AutoProxyRegistrar
+ *          ProxyTransactionManagementConfiguration
+ *
+ *      2.AutoProxyRegistrar：
+ *          给容器中注册 InfrastructureAdvisorAutoProxyCreator
+ *          利用后置处理器机制在对象创建以后包装对象，返回一个代理对象（增强器），代理对象执行方法利用拦截器链执行
+ *
+ *      3.ProxyTransactionManagementConfiguration 做了什么？
+ *          给容器中注册事务增强器，
+ *          a.事务增强器用事务注解的信息，AnnotationTransactionAttributeSource解析事务注解
+ *          b.事务拦截器
+ *              transactionInterceptor 保存了事务属性信息，事务管理器
+ *              他是一个MethodInterceptor
+ *              在目标方法执行的时候执行拦截器链
+ *              1).先获取事务相关的属性 TransactionAttribute
+ *              2).再获取 PlatformTransactionManager，如果事先没有添加指定任何TransactionManager
+ *                  最终会从容器中获取一个PlatformTransactionManager
+ *              3).执行目标方法
+ *                  如果异常，获取到事务管理器，利用事务管理器回滚这次操作
+ *                  如果正常：利用事务管理器提交事务
  *
  */
 @EnableTransactionManagement
